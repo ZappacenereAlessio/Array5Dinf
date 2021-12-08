d436f52e6b90fe7acdef37cbb7e673f65bba72d3
Zapp <Alessiozappacenere06@outlook.it>
2021-12-08 23:54:19 +0100

    popolare ListView con array

--- app/src/main/java/com/example/array5dinf/MainActivity.java
+++ app/src/main/java/com/example/array5dinf/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.array5dinf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String nazioni[] = {"Italia", "Francia", "Germania", "Olanda" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> aaStati = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, nazioni);
+
+        ListView listaStati = (ListView)findViewById(R.id.lvStati);
+        listaStati.setAdapter(aaStati);
     }
 }
