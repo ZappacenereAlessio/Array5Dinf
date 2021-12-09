b6faedc4bb7b7f46dc00fd83109acae651911c03
ZappacenereAlessio <alessiozappacenere06@outlook.it>
2021-12-09 11:40:18 +0100

    Button with Toast

--- app/src/main/java/com/example/array5dinf/MainActivity.java
+++ app/src/main/java/com/example/array5dinf/MainActivity.java
@@ -3,22 +3,35 @@ package com.example.array5dinf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String nazioni[] = {"Italia", "Francia", "Germania", "Olanda" };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn =(Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hai pemuto il tasto", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
         ArrayAdapter<String> aaStati = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, nazioni);
 
-        ListView listaStati = (ListView)findViewById(R.id.lvStati);
-        listaStati.setAdapter(aaStati);
+      /*  ListView listaStati = (ListView)findViewById(R.id.lvStati);
+        listaStati.setAdapter(aaStati); */
     }
 }
